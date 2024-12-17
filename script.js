7d70f0186578e4d9b2f58a2bf2f5dd671d9a0d0e
Sabrina Rodrigues <sabrinnasrodrigues@gmail.com>
2024-12-17 13:15:39 -0500

    added a name + new winning counter method + adjustments

--- script.js
+++ script.js
@@ -3,35 +3,15 @@ const board = document.querySelector("[data-board]");
 const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
 const winningMessage = document.querySelector("[data-winning-message]");
 const restartButton = document.querySelector("[data-restart-button]");
-const xScore = document.querySelector(".X-score");
-const oScore = document.querySelector(".O-score");
-let currentXScore = localStorage.getItem("currentXScore") || 0;
-let currentOScore = localStorage.getItem("currentOScore") || 0;
+const resetButton = document.querySelector(".reset-button");
+const xScoreBoardPoints = document.querySelectorAll(".point-x");
+const oScoreBoardPoints = document.querySelectorAll(".point-o");
 const maxScore = 5;
-const oScoreBoardPoints = document.querySelectorAll(".O-score")[0].children;
-const xScoreBoardPoints = document.querySelectorAll(".X-score")[0].children;
 
-if (currentOScore > 0 || currentXScore > 0) {
-    let oldOScore = currentOScore
-    let oldXScore = currentXScore
-    for (let index in oScoreBoardPoints) {
-        if (index < currentOScore) {
-            const scoreboardPointPosition = oScoreBoardPoints.length - oldOScore
-            oScoreBoardPoints[scoreboardPointPosition].innerText = "o"
-            oldOScore--
-        }
-    }
-    for (let index in xScoreBoardPoints) {
-        if (index < currentXScore) {
-            const scoreboardPointPosition = xScoreBoardPoints.length - oldXScore
-            xScoreBoardPoints[scoreboardPointPosition].innerText = "x"
-            oldXScore--
-        }
-    }
-}
-    
-let audio = new Audio();
+let currentXScore = JSON.parse(localStorage.getItem("currentXScore")) || 0;
+let currentOScore = JSON.parse(localStorage.getItem("currentOScore")) || 0;
 
+let audio = new Audio()
 let isCircleTurn;
 
 const winningCombinations = [
@@ -43,144 +23,167 @@ const winningCombinations = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-];
+]
 
-const reset = () => {
-    currentXScore = 0
-    currentOScore = 0
-    localStorage.setItem("currentXScore", currentXScore)
-    localStorage.setItem("currentOScore", currentOScore)
+const loadSavedGameState = () => {
+    applySavedPoints(xScoreBoardPoints, currentXScore, "x")
+    applySavedPoints(oScoreBoardPoints, currentOScore, "o")
+    checkGameOver()
+}
+
+const applySavedPoints = (scoreBoardPoints, score, player) => {
+    for (let i = 0; i < score; i++) {
+        scoreBoardPoints[scoreBoardPoints.length - 1 - i].innerText = player;
+        scoreBoardPoints[scoreBoardPoints.length - 1 - i].classList.add("winning")
+    }
+}
+
+const saveGameState = () => {
+    localStorage.setItem("currentXScore", JSON.stringify(currentXScore))
+    localStorage.setItem("currentOScore", JSON.stringify(currentOScore))
+}
+
+const resetGame = () => {
+    currentXScore = 0;
+    currentOScore = 0;
+    saveGameState()
+
+    xScoreBoardPoints.forEach(point => point.classList.remove("winning"))
+    oScoreBoardPoints.forEach(point => point.classList.remove("winning"))
+    xScoreBoardPoints.forEach(point => point.innerText = "")
+    oScoreBoardPoints.forEach(point => point.innerText = "")
+
+    resetButton.classList.add("hidden")
     restartButton.classList.remove("hidden")
-    Array.from(oScoreBoardPoints).forEach(point => {
-        point.innerText = ""
-    });
-    Array.from(xScoreBoardPoints).forEach(point => {
-        point.innerText = ""
-    });
     startGame()
 }
 
 const startGame = () => {
     isCircleTurn = false;
-    audio = new Audio('sounds/start.mp3')
-    audio.play();
+    audio = new Audio("sounds/start.mp3")
+    audio.play()
 
     for (const cell of cellElements) {
-        cell.classList.remove("circle");
-        cell.classList.remove("x");
-        cell.removeEventListener("click", handleClick);
-        cell.addEventListener("click", handleClick, { once: true });
+        cell.classList.remove("circle", "x")
+        cell.removeEventListener("click", handleClick)
+        cell.addEventListener("click", handleClick, { once: true })
     }
 
     setBoardHoverClass()
-    winningMessage.classList.remove("show-winning-message");
-};
+    winningMessage.classList.remove("show-winning-message")
+}
 
 const endGame = (isDraw) => {
-   if (isDraw) {
-    winningMessageTextElement.innerText = "Draw!"
-   } else {
-    winningMessageTextElement.innerText = isCircleTurn ? "O's Wins!" : "X's Wins!";
-    if(isCircleTurn) {
-        currentOScore++
-        const scoreboardPointPosition = oScoreBoardPoints.length - currentOScore
-        oScoreBoardPoints[scoreboardPointPosition].innerText = "o"
-        localStorage.setItem("currentOScore", currentOScore)
+    if (isDraw) {
+        winningMessageTextElement.innerText = "Draw!";
     } else {
-        currentXScore++
-        const scoreboardPointPosition = oScoreBoardPoints.length - currentXScore
-        xScoreBoardPoints[scoreboardPointPosition].innerText = "x"
-        localStorage.setItem("currentXScore", currentXScore)
+        winningMessageTextElement.innerText = isCircleTurn ? "O's Wins!" : "X's Wins!";
+        updateScore(isCircleTurn ? oScoreBoardPoints : xScoreBoardPoints, isCircleTurn ? "o" : "x")
+        saveGameState()
     }
 
-    if(currentXScore >= maxScore || currentOScore >= maxScore) {
+    winningMessage.classList.add("show-winning-message")
+    checkGameOver()
+}
+
+const updateScore = (scoreBoardPoints, player) => {
+    if (player === "x") {
+        currentXScore++;
+        if (currentXScore <= maxScore) {
+            markWinningPoint(scoreBoardPoints, player)
+        }
+    } else {
+        currentOScore++;
+        if (currentOScore <= maxScore) {
+            markWinningPoint(scoreBoardPoints, player)
+        }
+    }
+}
+
+const markWinningPoint = (scoreBoardPoints, player) => {
+    for (const point of [...scoreBoardPoints].reverse()) {
+        if (!point.innerText) {
+            point.innerText = player;
+            point.classList.add("winning")
+            break;
+        }
+    }
+}
+
+const checkGameOver = () => {
+    if (currentXScore >= maxScore || currentOScore >= maxScore) {
         restartButton.classList.add("hidden")
+        resetButton.classList.remove("hidden")
     }
-
-   }
-
-   winningMessage.classList.add("show-winning-message")
 }
 
 const checkForWin = (currentPlayer) => {
     return winningCombinations.some(combination => {
-        return combination.every(index => {
-            return cellElements[index].classList.contains(currentPlayer);
-        })
+        return combination.every(index => cellElements[index].classList.contains(currentPlayer))
     })
 }
 
 const checkForDraw = () => {
-    return [ ... cellElements].every(cell => {
-       return cell.classList.contains("x") || cell.classList.contains("circle")
-    })
+    return [...cellElements].every(cell => cell.classList.contains("x") || cell.classList.contains("circle"))
 }
 
 const placeMark = (cell, classToAdd) => {
-    cell.classList.add(classToAdd);
-};
+    cell.classList.add(classToAdd)
+}
 
 const setBoardHoverClass = () => {
-    board.classList.remove("circle");
-    board.classList.remove("x");
-    
-    if (isCircleTurn) {
-        board.classList.add("circle");
-    } else {
-        board.classList.add("x");
-    }
+    board.classList.remove("circle", "x")
+    board.classList.add(isCircleTurn ? "circle" : "x")
 }
 
 const swapTurns = () => {
     isCircleTurn = !isCircleTurn;
-
-    setBoardHoverClass();
+    setBoardHoverClass()
 }
 
 const handleClick = (e) => {
-    // Colocar a marca (X or circulo)
     const cell = e.target;
     const classToAdd = isCircleTurn ? "circle" : "x";
 
-    if (isCircleTurn) {
-        audio = new Audio('sounds/circle.mp3')
-        audio.play()
-    } else { 
-        audio = new Audio('sounds/x.mp3')
-        audio.play();
-    }
+    audio = new Audio(isCircleTurn ? "sounds/circle.mp3" : "sounds/x.mp3");
+    audio.play()
 
-    placeMark(cell, classToAdd);    
+    placeMark(cell, classToAdd)
 
-    // Verificar por vitoria
-    const isWin = checkForWin(classToAdd);
-    
-    // Verificar por empate
-    const isDraw = checkForDraw();
+    const isWin = checkForWin(classToAdd)
+    const isDraw = checkForDraw()
 
     if (isWin) {
         endGame(false)
-        setTimeout(() => {
-            audio.pause()
-            audio.currentTime = 0
-            audio = new Audio('sounds/win.mp3')
-            return audio.play()
-        }, 600)  
-
+        playWinSound()
     } else if (isDraw) {
         endGame(true)
-        setTimeout(() => {
-            audio.pause()
-            audio.currentTime = 0
-            audio = new Audio('sounds/draw.mp3')
-            return audio.play()
-        }, 600)  
+        playDrawSound()
     } else {
-    // Mudar o simbolo
-        swapTurns();
+        swapTurns()
     }
-};
+}
 
-startGame();
+const playWinSound = () => {
+    setTimeout(() => {
+        audio.pause()
+        audio.currentTime = 0;
+        audio = new Audio("sounds/win.mp3")
+        audio.play()
+    }, 600);
+}
 
-restartButton.addEventListener("click", startGame);
+const playDrawSound = () => {
+    setTimeout(() => {
+        audio.pause()
+        audio.currentTime = 0;
+        audio = new Audio("sounds/draw.mp3")
+        audio.play()
+    }, 600)
+}
+
+restartButton.addEventListener("click", startGame)
+resetButton.addEventListener("click", resetGame)
+
+loadSavedGameState()
+startGame()
